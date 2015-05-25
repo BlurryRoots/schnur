67adccb16bdbaf2803f6acb60b7f46bb715a1ded
blurryroots <blurryroots@posteo.de>
2015-05-25 18:38:22 +0200

    tabs

--- test/string-library.c
+++ test/string-library.c
@@ -18,12 +18,12 @@ limitations under the License.
 #include <catch.hpp>
 
 extern "C" {
-    #include "../src/string-library.h"
+	#include "../src/string-library.h"
 
-    #include <stdlib.h>
-    #include <stdio.h>
-    #include <wchar.h>
-    #include <locale.h>
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <wchar.h>
+	#include <locale.h>
 }
 
 #include <iostream>
@@ -34,178 +34,178 @@ TEST_CASE ("string util", "[string]") {
 }
 
 TEST_CASE ("string manipulation", "[string]") {
-    string_t* s = NULL;
+	string_t* s = NULL;
 
-    s = string_new ();
-    REQUIRE (NULL != s);
-
-#if 1 == SHOW_DEBUG
-    setlocale (LC_ALL, "");
-#endif
-
-    SECTION ("string_new") {
-        REQUIRE (NULL != s->data);
-        REQUIRE (STRING_BLOCK_SIZE == s->capacity);
-        REQUIRE (0 == s->length);
-    }
-
-    SECTION ("string_expand") {
-        string_expand (s);
-
-        REQUIRE (NULL != s->data);
-        REQUIRE ((2 * STRING_BLOCK_SIZE) == s->capacity);
-    }
-
-    SECTION ("string_compact") {
-        string_expand (s);
-        string_expand (s); // 3 * STRING_BLOCK_SIZE
-
-        REQUIRE (NULL != s->data);
-        REQUIRE ((3 * STRING_BLOCK_SIZE) == s->capacity);
-
-        string_fill (s, L'=');
-
-        s->length =
-            (2 * STRING_BLOCK_SIZE - 1)
-            - (STRING_BLOCK_SIZE / 2);
-        // produces a memory offset of half the block size
-        // and freeable memory of 1 * STRING_BLOCK_SIZE
-        s->data[s->length] = '\0';
-
-        REQUIRE (1 == string_compact (s));
-        REQUIRE ((2 * STRING_BLOCK_SIZE) == s->capacity);
-    }
-
-    SECTION ("string_copy_cstr") {
-        const wchar_t* w = L"Hänsel mag Soße!";
-        const size_t l =  wcslen (w);
-
-        REQUIRE (wcslen (w) == l);
-        string_copy_cstr (s, w);
-
-        REQUIRE (0 == wcsncmp (s->data, w, l));
-    }
-
-    SECTION ("string_copy") {
-        const wchar_t* w = L"ευχαριστημένος";
-        const size_t l = wcslen (w);
-        string_t* o;
-
-        REQUIRE (wcslen (w) == l);
-        string_copy_cstr (s, w);
-
-        REQUIRE (0 == wcsncmp (s->data, w, l));
-
-        o = string_new ();
-        REQUIRE (NULL != o);
-
-        string_copy (o, s);
-        REQUIRE (0 == wcsncmp (s->data, o->data, l));
-
-        string_free (o);
-    }
-
-    SECTION( "string_append" ) {
-        const wchar_t* b = L"?";
-
-        REQUIRE (1 == string_append (s, b[0]));
-
-        REQUIRE (0 == wcsncmp (s->data, b, 1));
-    }
-
-    SECTION ("string_append mass") {
-        const int Count = 23000;
-        const wchar_t* t = L"0123456789";
-
-        for (int i = 0; i < Count; ++i) {
-            REQUIRE (1 == string_append (s, t[i % 10]));
-        }
-
-        for (int i = 0; i < Count; ++i) {
-            REQUIRE (s->data[i] == t[i % 10]);
-        }
-    }
-
-    SECTION ("string_append_cstr") {
-        const wchar_t* a = L"Viel";
-        const wchar_t* b = L" Spaß!";
-        const wchar_t* both = L"Viel Spaß!";
-        const size_t l = wcslen( both );
-
-        string_copy_cstr (s, a);
-
-        REQUIRE (1 == string_append_cstr (s, b));
-
-        REQUIRE (0 == wcsncmp (s->data, both, l));
-    }
-
-    SECTION ("string_append_string") {
-        const wchar_t* a = L"Viel";
-        const wchar_t* b = L" Spaß!";
-        const wchar_t* both = L"Viel Spaß!";
-        const size_t l = wcslen (both);
-
-        string_t* s2;
-
-        string_copy_cstr (s, a);
-
-        s2 = string_new ();
-        REQUIRE (NULL != s2);
-        string_copy_cstr (s2, b);
-
-        REQUIRE (1 == string_append_string (s, s2));
-
-        REQUIRE (0 == wcsncmp (s->data, both, l));
-
-        string_free (s2);
-    }
-
-    SECTION ("string_equal_cstr") {
-        const wchar_t* w = L"舒适";
-
-        string_copy_cstr (s, w);
-
-        REQUIRE (1 == string_equal_cstr (s, w));
-    }
-
-    SECTION ("string_equal") {
-        const wchar_t* w = L"décontracté";
-        string_t* o;
-
-        string_copy_cstr (s, w);
-
-        o = string_new ();
-        REQUIRE (NULL != o);
-        string_copy_cstr (o, w);
-
-        REQUIRE (s != o);
-
-        REQUIRE (1 == string_equal (s, o));
-
-        string_free (o);
-    }
-
-    SECTION ("string_reverse") {
-        const wchar_t* w = L"солнце";
-        const size_t l = wcslen (w);
-
-        string_copy_cstr (s, w);
+	s = string_new ();
+	REQUIRE (NULL != s);
 
 #if 1 == SHOW_DEBUG
-        wprintf (L"d: %ls\n", s->data);
+	setlocale (LC_ALL, "");
 #endif
 
-        REQUIRE (1 == string_reverse (s));
+	SECTION ("string_new") {
+		REQUIRE (NULL != s->data);
+		REQUIRE (STRING_BLOCK_SIZE == s->capacity);
+		REQUIRE (0 == s->length);
+	}
+
+	SECTION ("string_expand") {
+		string_expand (s);
+
+		REQUIRE (NULL != s->data);
+		REQUIRE ((2 * STRING_BLOCK_SIZE) == s->capacity);
+	}
+
+	SECTION ("string_compact") {
+		string_expand (s);
+		string_expand (s); // 3 * STRING_BLOCK_SIZE
+
+		REQUIRE (NULL != s->data);
+		REQUIRE ((3 * STRING_BLOCK_SIZE) == s->capacity);
+
+		string_fill (s, L'=');
+
+		s->length =
+			(2 * STRING_BLOCK_SIZE - 1)
+			- (STRING_BLOCK_SIZE / 2);
+		// produces a memory offset of half the block size
+		// and freeable memory of 1 * STRING_BLOCK_SIZE
+		s->data[s->length] = '\0';
+
+		REQUIRE (1 == string_compact (s));
+		REQUIRE ((2 * STRING_BLOCK_SIZE) == s->capacity);
+	}
+
+	SECTION ("string_copy_cstr") {
+		const wchar_t* w = L"Hänsel mag Soße!";
+		const size_t l =  wcslen (w);
+
+		REQUIRE (wcslen (w) == l);
+		string_copy_cstr (s, w);
+
+		REQUIRE (0 == wcsncmp (s->data, w, l));
+	}
+
+	SECTION ("string_copy") {
+		const wchar_t* w = L"ευχαριστημένος";
+		const size_t l = wcslen (w);
+		string_t* o;
+
+		REQUIRE (wcslen (w) == l);
+		string_copy_cstr (s, w);
+
+		REQUIRE (0 == wcsncmp (s->data, w, l));
+
+		o = string_new ();
+		REQUIRE (NULL != o);
+
+		string_copy (o, s);
+		REQUIRE (0 == wcsncmp (s->data, o->data, l));
+
+		string_free (o);
+	}
+
+	SECTION( "string_append" ) {
+		const wchar_t* b = L"?";
+
+		REQUIRE (1 == string_append (s, b[0]));
+
+		REQUIRE (0 == wcsncmp (s->data, b, 1));
+	}
+
+	SECTION ("string_append mass") {
+		const int Count = 23000;
+		const wchar_t* t = L"0123456789";
+
+		for (int i = 0; i < Count; ++i) {
+			REQUIRE (1 == string_append (s, t[i % 10]));
+		}
+
+		for (int i = 0; i < Count; ++i) {
+			REQUIRE (s->data[i] == t[i % 10]);
+		}
+	}
+
+	SECTION ("string_append_cstr") {
+		const wchar_t* a = L"Viel";
+		const wchar_t* b = L" Spaß!";
+		const wchar_t* both = L"Viel Spaß!";
+		const size_t l = wcslen( both );
+
+		string_copy_cstr (s, a);
+
+		REQUIRE (1 == string_append_cstr (s, b));
+
+		REQUIRE (0 == wcsncmp (s->data, both, l));
+	}
+
+	SECTION ("string_append_string") {
+		const wchar_t* a = L"Viel";
+		const wchar_t* b = L" Spaß!";
+		const wchar_t* both = L"Viel Spaß!";
+		const size_t l = wcslen (both);
+
+		string_t* s2;
+
+		string_copy_cstr (s, a);
+
+		s2 = string_new ();
+		REQUIRE (NULL != s2);
+		string_copy_cstr (s2, b);
+
+		REQUIRE (1 == string_append_string (s, s2));
+
+		REQUIRE (0 == wcsncmp (s->data, both, l));
+
+		string_free (s2);
+	}
+
+	SECTION ("string_equal_cstr") {
+		const wchar_t* w = L"舒适";
+
+		string_copy_cstr (s, w);
+
+		REQUIRE (1 == string_equal_cstr (s, w));
+	}
+
+	SECTION ("string_equal") {
+		const wchar_t* w = L"décontracté";
+		string_t* o;
+
+		string_copy_cstr (s, w);
+
+		o = string_new ();
+		REQUIRE (NULL != o);
+		string_copy_cstr (o, w);
+
+		REQUIRE (s != o);
+
+		REQUIRE (1 == string_equal (s, o));
+
+		string_free (o);
+	}
+
+	SECTION ("string_reverse") {
+		const wchar_t* w = L"солнце";
+		const size_t l = wcslen (w);
+
+		string_copy_cstr (s, w);
 
 #if 1 == SHOW_DEBUG
-        wprintf (L"d: %ls\n", s->data);
+		wprintf (L"d: %ls\n", s->data);
 #endif
 
-        for (size_t i = 0; i < l; ++i) {
-            REQUIRE (s->data[i] == w[l - i - 1]);
-        }
-    }
+		REQUIRE (1 == string_reverse (s));
 
-    // free string for all test
-    string_free (s);
+#if 1 == SHOW_DEBUG
+		wprintf (L"d: %ls\n", s->data);
+#endif
+
+		for (size_t i = 0; i < l; ++i) {
+			REQUIRE (s->data[i] == w[l - i - 1]);
+		}
+	}
+
+	// free string for all test
+	string_free (s);
 }
